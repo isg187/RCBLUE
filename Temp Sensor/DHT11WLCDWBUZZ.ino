2941858502ac45b7a3adb4c2eafba6250c3d210c
Isidro Sandoval <isg187@yahoo.com>
2017-10-26 19:21:29 -0600

    DHT11 Temperature and Humidity sensor with LCD and Buzzer

---
+++ Temp Sensor/DHT11WLCDWBUZZ.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h>
+#include <dht.h>
+ 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int greenPin = 13;
+dht sensor;
+int buzz = 10;
+ 
+void setup()
+{
+  lcd.begin(16,2); //16 by 2 character display
+  pinMode(buzz,OUTPUT);
+  digitalWrite(buzz,LOW);
+}
+ 
+void loop()
+{
+  delay(1000); //wait a sec (recommended for DHT11)
+  sensor.read11(greenPin);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Humidity = ");
+  lcd.print(sensor.humidity);
+  lcd.setCursor(0,1);
+  lcd.print("Temp = ");
+  lcd.print(sensor.temperature*1.8+32);
+  float t = (sensor.temperature*1.8+32);
+    {
+      if (t>30)
+      {
+            digitalWrite(buzz,HIGH);
+      } else
+              digitalWrite(buzz,LOW);
+}}
